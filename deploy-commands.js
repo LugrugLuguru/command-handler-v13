044a70aebb4162acf44c8a39e337136e1495fedc
Nico <67909777+nilasystem@users.noreply.github.com>
2022-07-02 18:11:50 +0200

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 const { REST } = require('@discordjs/rest');
 const { Routes } = require('discord-api-types/v10');
-const { clientId, guildId, token } = require('./config/config.json');
+const { clientId, token } = require('./config/config.json');
 
 const commands = [];
 const commandsPath = path.join(__dirname, 'commands');
@@ -17,7 +17,7 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '10' }).setToken(token);
 
-rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
+rest.put(Routes.applicationCommands(clientId), { body: commands })
 	.then(() => console.log('Successfully registered application commands.'))
 	.catch(console.error);
 
